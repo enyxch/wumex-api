f0be1e0d65f3edf95070a79856e1cd5644f0a355
Niels Liebisch <niels@optimalconsulting.com>
2014-05-22 11:30:34 +0200

    projects first

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -1,8 +1,8 @@
 module API
   module V1
     class Base < Grape::API
-      mount API::V1::Projects
       mount API::V1::UserRegistration
+      mount API::V1::Projects
       add_swagger_documentation base_path: "/api",
                                 api_version: 'v1',
                                 hide_documentation_path: true
