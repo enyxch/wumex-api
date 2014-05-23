bb68e857a478454b229b0d894ac3d906accc4552
Niels Liebisch <niels@optimalconsulting.com>
2014-05-23 15:17:30 +0200

    added user data

--- db/migrate/20140522052352_create_users.rb
+++ db/migrate/20140522052352_create_users.rb
@@ -4,6 +4,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :user_name
       t.string :authentication_token
+      t.string :password
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20140522052352) do
     t.string   "email"
     t.string   "user_name"
     t.string   "authentication_token"
+    t.string   "password"
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
