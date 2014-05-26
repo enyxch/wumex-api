9c583b89541269050bfbfe0f2ae8100254bf1530
Niels Liebisch <niels@optimalconsulting.com>
2014-05-26 10:11:11 +0200

    root to api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   mount API::Base => '/api'
 
+  root :to => redirect('/api/docs')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
