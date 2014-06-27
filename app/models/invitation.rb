a04f6203718e507218c6f07202e383bd76539fb5
Niels Liebisch <niels.liebisch@enyx.ch>
2014-06-27 16:59:11 +0530

    Added method and specs for Invitation creation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,4 +3,15 @@ class Invitation < ActiveRecord::Base
   belongs_to :user
   belongs_to :inviting_user, :class_name => 'User', :foreign_key => 'inviting_user_id'
   
+  class << self
+    def create_invitation(params, inviting_user_id)
+      self.create({ :notes => params[:notes],
+        :project_id => params[:project_id],
+        :user_id => params[:user_id],
+        :inviting_user_id => inviting_user_id
+      })
+    end
+  end
+  
+  
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe Invitation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:inviting_user).class_name('User').with_foreign_key('inviting_user_id') }
+  end
+  
+  it 'has a valid factory' do
+    FactoryGirl.create(:invitation).should be_valid
+  end
+  
+  describe "#create" do
+    before(:each) do
+      @inviting_user = FactoryGirl.create(:user)
+      @invited_user = FactoryGirl.create(:user)
+      @project = FactoryGirl.create(:project)
+    end
+      
+    it 'should be created' do
+      params = invitation_params( :project_id => @project.id, :user_id => @invited_user.id )
+      Invitation.create_invitation(params, @inviting_user.id).persisted?.should be_true
+    end
+    
+    it 'should be created without project' do
+      params = invitation_params( :project_id => nil, :user_id => @invited_user.id )
+      Invitation.create_invitation(params, @inviting_user.id).persisted?.should be_true
+    end
+    
+    it 'should be created without invited user' do
+      params = invitation_params( :project_id => @project.id, :user_id => nil )
+      Invitation.create_invitation(params, @inviting_user.id).persisted?.should be_true
+    end
+    
+    it 'should be created without inviting user' do
+      params = invitation_params( :project_id => @project.id, :user_id => @invited_user.id )
+      Invitation.create_invitation(params, nil).persisted?.should be_true
+    end
+    
+    def invitation_params opts = {}
+      {
+        :notes =>"Test",
+        :project_id =>1,
+        :user_id =>1
+      }.merge(opts)
+    end
+  end
+  
 end
