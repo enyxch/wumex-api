e7f002d772ae998fafa85bafe50fa74c0c0eb42d
Niels Liebisch <niels.liebisch@enyx.ch>
2014-07-02 18:07:24 +0530

    Added End point to Invite list of Users, and create invitations for all user IDs

--- app/controllers/api/v1/invitations.rb
+++ app/controllers/api/v1/invitations.rb
@@ -14,21 +14,21 @@ module API
           current_user.invitations
         end
       
-        desc "Authorize User can create Invitations"
+        desc "Authorize User can send Project Invitations"
         params do
           requires :token, type: String, desc: "Authorization"
           optional :notes, type: String
           requires :project_id, type: Integer
           requires :user_id, type: Integer
         end
-        post :create do
+        post :project_invitation do
           project = current_user.projects.find_by_id(params[:project_id])
           return error!({:error_code => ErrorList::PROJECT_NOT_FOUND, :error_message => "Could not find project"}, 404) unless project
           
           user = User.find_by_id(params[:user_id])
           return error!({:error_code => ErrorList::USER_NOT_FOUND, :error_message => "Could not find user"}, 404) unless user
           
-          invitation = Invitation.create_invitation(params, current_user.id)
+          invitation = Invitation.create_project_invitation(params, current_user.id)
           if invitation.persisted?
             status(201)
             {
@@ -40,12 +40,12 @@ module API
           end
         end
         
-        desc "Accept Invitations"
+        desc "Accept Project Invitations"
         params do
           requires :token, type: String, desc: "Authorization"
           requires :invitation_id, type: Integer
         end
-        put :accept_invitation do
+        put :accept_project_invitation do
           invitation = current_user.invitations.get_invitation(params[:invitation_id]).first
           return error!({:error_code => ErrorList::INVITATION_NOT_FOUND, :error_message => "Could not find invitation"}, 404) unless invitation
           
@@ -63,6 +63,34 @@ module API
             end
           else
             error!({:error_code => ErrorList::PROJECT_NOT_UPDATED, :error_message => project.errors.full_messages.to_s}, 422)
+          end
+        end
+        
+        desc "Authorize User can send Contact Invitations"
+        params do
+          requires :token, type: String, desc: "Authorization"
+          group :user_ids
+        end
+        post :contact_invitation do
+          user_ids = eval(params[:user_ids])
+          
+          users = User.where(id: user_ids)
+          not_found_user_ids = user_ids.select{|user_id| users.map(&:id).exclude?(user_id.to_i) }
+          return error!({:error_code => ErrorList::USER_NOT_FOUND, :error_message => "Could not find users with following IDs #{not_found_user_ids}"}, 404) if not_found_user_ids.present?
+          
+          invitations = []
+          users.each do |user|
+            invitation = Invitation.create_contact_invitation(user.id, current_user.id)
+            error!({:error_code => ErrorList::INVITATION_NOT_CREATED, :error_message => invitation.errors.full_messages.to_s}, 422) unless invitation.persisted?
+            invitations << invitation.id
+          end
+          
+          if invitations.present?
+            status(200)
+            { 
+              invitations: invitations,
+              status: 'ok'
+            }
           end
         end
         
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -6,14 +6,21 @@ class Invitation < ActiveRecord::Base
   scope :get_invitation, ->(id) { where(:id => id) }
   
   class << self
-    def create_invitation(params, inviting_user_id)
+    def create_project_invitation(params, inviting_user_id)
       self.create({ :notes => params[:notes],
         :project_id => params[:project_id],
         :user_id => params[:user_id],
         :inviting_user_id => inviting_user_id
       })
     end
+    
+    def create_contact_invitation(user_id, inviting_user_id)
+      self.create({ :user_id => user_id,
+        :inviting_user_id => inviting_user_id
+      })
+    end
   end
+  
   
   
 end
