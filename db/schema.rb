49fec415aa50f7b0cfaa894fc561dd572bc024b2
Niels Liebisch <niels.liebisch@enyx.ch>
2014-07-02 19:00:04 +0530

    Created migration to add owner_id column in Projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630074128) do
+ActiveRecord::Schema.define(version: 20140702125007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20140630074128) do
     t.integer  "percent_done"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "owner_id"
   end
 
   create_table "projects_users", id: false, force: true do |t|
